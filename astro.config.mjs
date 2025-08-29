72112aaab76d90b06c55908042d378411670031d
yingjunnan <yingjunnan@outlook.com>
2025-08-29 10:55:39 +0800

    chore: 更新astro配置中的站点URL为devops子域名

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://ok.imok.xyz/",
+	site: "https://devops.imok.xyz/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
