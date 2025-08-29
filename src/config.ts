e3191f7a0be10bd9c84fee0e5756503e13c44ef5
yingjunnan <yingjunnan@outlook.com>
2025-08-29 10:51:24 +0800

    docs(config): 更新个人资料配置中的姓名和简介
    将默认的占位姓名和简介更新为实际用户信息

--- src/config.ts
+++ src/config.ts
@@ -54,8 +54,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	name: "Junnan Ying",
+	bio: "Junnan Ying dolor sit amet, consectetur adipiscing elit.",
 	links: [
 		{
 			name: "Twitter",
